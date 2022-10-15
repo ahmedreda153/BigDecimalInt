72fb386a6137461dddff5288602f80a97fdf461a
Reda <ahmedreda5338@gmail.com>
2022-10-15 21:46:19 +0200

    test

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-    BigDecimalInt c1("750");
+    BigDecimalInt c1("7");
     BigDecimalInt c2("-75");
     BigDecimalInt c3 = c1 + c2;
     // BigDecimalInt c3 = c1 < c2;
